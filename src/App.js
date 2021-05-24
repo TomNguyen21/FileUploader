ff16daa9b42be3862753f30b88efcf62a6e97f21
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-05-24 00:23:56 -0700

    Make logo a local asset

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import FileUpload from './FileUpload';
 import UploadedFile from './UploadedFile';
+import logo from './Wefunder_logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="weFunder_logo" src="https://upload.wikimedia.org/wikipedia/commons/e/e6/Wefunder_logo.png" alt="WeFunder" />
+        <img className="weFunder_logo" src={logo} alt="WeFunder" />
         <FileUpload />
         <div className="pitch_deck">
         <UploadedFile />
