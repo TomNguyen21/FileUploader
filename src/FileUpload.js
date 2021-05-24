59de371e9f065c54e57ce520283681f89dcd3f54
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-05-23 23:28:32 -0700

    Change how input is displayed

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -4,11 +4,16 @@ import './FileUpload.css';
 
 function FileUpload() {
   const [fileData, setFileData] = useState("");
+  const [fileName, setFileName] = useState("");
 
   const getFile = (e) => {
+    console.log(e.target.files[0])
     setFileData(e.target.files[0]);
   };
 
+  const getFileName = (e) => {
+    setFileName(e.target.files[0].name)
+  }
   const uploadFile = (e) => {
     e.preventDefault();
     const data = new FormData();
@@ -25,9 +30,13 @@ function FileUpload() {
     <form onSubmit={uploadFile}>
       <div className="file_inputs">
       <label className="file_input">
-        <input type="file" name="file" onChange={getFile} required />
+        <input type="file" name="file" onChange={(e) => {
+          getFile(e);
+          getFileName(e);
+        }} required />
         Choose File
       </label>
+      {fileName ? fileName : ""}
       <label className="file_input">
         <input type="submit" name= "upload" value= "Upload" />
         Upload
--- src/UploadedFile.js
+++ src/UploadedFile.js
@@ -23,16 +23,18 @@ function UploadedFile() {
         }}
         file={file}
         onLoadSuccess={onDocumentLoadSuccess}
-      >{Array.from(
-        new Array(numPages),
-        (el, index) => (
+      >
+        {Array.from(
+          new Array(numPages),
+          (el, index) => (
           <Page
             key={`page_${index + 1}`}
             pageNumber={index + 1}
           />
         ),
-      )}
+        )}
       </Document>
+
     </div>
   )
 }
